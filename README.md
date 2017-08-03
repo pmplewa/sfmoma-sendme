4e9a1ddc851e6fd021a3fb42e9765b0c2bdda210
Philipp Plewa <pmplewa@users.noreply.github.com>
2017-08-03 10:13:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[Example Notebook](example.ipynb)
---
+++ art/sfmoma.py
@@ -0,0 +1,58 @@
+from IPython.core.display import display, HTML, Image
+import numpy as np
+import requests
+from urllib.parse import urljoin
+
+# check if running in a jupyter notebook
+try:
+    shell = get_ipython().__class__.__name__
+    if shell != "ZMQInteractiveShell":
+        raise Exception(shell)
+except:
+    raise
+
+class connect():
+    def __init__(self, token, root_url="https://www.sfmoma.org/api/collection",
+                 resource_url="artworks"):
+        self.url = urljoin(f"{root_url}/", f"{resource_url}/")
+        self.auth_header = {"Authorization": f"Token {token}"}
+        
+    def request(self, payload):
+        req = requests.get(self.url, params=payload, headers=self.auth_header)   
+        req.raise_for_status()
+        return req.json()
+        
+    def get_item(self, keyword):
+        payload = {}
+        payload["object_keywords__regex"] = f"\m{keyword.lower()}\M"
+        payload["page_size"] = 20
+        payload["has_images"] = True
+        
+        json = self.request(payload)
+        if json["count"] == 0:
+            raise Exception("No items found.")
+        
+        item_index = np.random.randint(json["count"])
+        page_number = item_index // payload["page_size"]
+        page_number += 1 # first page has index 1 (not 0)
+        page_index = item_index % payload["page_size"]
+        
+        if page_number > 1:
+            payload = {**payload, **{"page": page_number}}
+            json = self.request(payload)
+        
+        item = json["results"][page_index]
+                
+        return item
+
+    def display_item(self, item):
+        image = item["images"][0]
+        display(Image(image["public_image"]))
+        display(HTML("<b>Caption:</b>"))
+        display(HTML(image["caption"]))
+        display(HTML("<b>Keywords:</b>"))
+        display(HTML(item["object_keywords"]))
+    
+    def send_me(self, keyword):
+        item = self.get_item(keyword)
+        self.display_item(item)
